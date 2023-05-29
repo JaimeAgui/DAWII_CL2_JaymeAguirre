7e2e45b5b0964e3d540d76fbe25b6875b3182bde
jaime <jaymeaguirre26@gmail.com>
2023-05-29 13:51:10 -0500

    inico del proyecto

---
+++ src/main/java/examen/DAWII_CL2_JaymeAguirre/model/bd/Usuario.java
@@ -0,0 +1,36 @@
+package examen.DAWII_CL2_JaymeAguirre.model.bd;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idusuario;
+    @Column(name = "nomusuario")
+    private String nomusuario;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "nombres")
+    private String nombres;
+    @Column(name = "apellidos")
+    private String apellidos;
+    @Column(name = "activo")
+    private Boolean activo;
+
+
+
+}
