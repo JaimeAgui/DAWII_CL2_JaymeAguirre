097c1fef7547c0d357bf2d606d5d161f9099558d
jaime <jaymeaguirre26@gmail.com>
2023-05-29 17:42:23 -0500

    login terminado

--- src/main/java/examen/DAWII_CL2_JaymeAguirre/model/bd/Usuario.java
+++ src/main/java/examen/DAWII_CL2_JaymeAguirre/model/bd/Usuario.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import java.util.Set;
 
 @NoArgsConstructor
@@ -18,19 +17,13 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idusuario;
-    @Column(name = "nomusuario")
-    private String nomusuario;
-    @Column(name = "email")
-    private String email;
-    @Column(name = "password")
-    private String password;
     @Column(name = "nombres")
     private String nombres;
     @Column(name = "apellidos")
     private String apellidos;
-    @Column(name = "activo")
-    private Boolean activo;
-
-
+    @Column(name = "nomusuario")
+    private String nomusuario;
+    @Column(name = "passusuario")
+    private String passusuario;
 
 }
